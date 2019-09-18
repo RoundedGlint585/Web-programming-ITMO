da4563882dce2e7bfc5fe953d1e8c646dd5bc72e
roundedglint585 <danonofficial@gmail.com>
2019-09-18 10:20:39 +0300

    Small fixes and refactoring

--- Lab1/weatherFetching.js
+++ Lab1/weatherFetching.js
@@ -1,41 +1,54 @@
 const key = '6f2aa31213f556b4d1b03a048629724f';
 
+
+const form = document.getElementById('inputForm');
+form.onsubmit = weatherFetch;
 // function to search for position
+
+function weatherRender(json){
+    var source   = document.getElementById("weatherTemplate").innerHTML;
+    var template = Handlebars.compile(source);
+    json['main']['temp'] -= 273.15;
+    if(json['main']['temp'] < 10){
+        json['main']['temp'] = json['main']['temp'] .toPrecision(2);
+    }else{
+        json['main']['temp'] = json['main']['temp'] .toPrecision(3);
+    }
+
+    var context = {
+        cityName: json['name'],
+        weatherType: json['weather'][0]['main'],
+        windSpeed: json['wind']['speed'],
+        humidity: json['main']['humidity'],
+        currTemp: json['main']['temp']};
+    document.getElementById('compiled').innerHTML = template(context);
+}
+
+function errorRender(errorText){
+    var source   = document.getElementById("errorTemplate").innerHTML;
+    var template = Handlebars.compile(source);
+
+    document.getElementById('compiled').innerHTML = template(context);
+}
 function weatherFetch(cityName){
     var cityName = document.getElementById("cityName").value;
     var url =  'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key;
     fetch(url).then(function(response) {
         if(response.ok) {
             response.json().then(function(json) {
-                var source   = document.getElementById("weatherTemplate").innerHTML;
-                var template = Handlebars.compile(source);
-                var temperature = json['main']['temp'] - 273.15;
-                if(temperature < 10){
-                    temperature = temperature.toPrecision(2);
-                }else{
-                    temperature = temperature.toPrecision(3);
-                }
-
-                var context = {
-                    cityName: json['name'],
-                    weatherType: json['weather'][0]['main'],
-                    windSpeed: json['wind']['speed'],
-                    humidity: json['main']['humidity'],
-                    currTemp: temperature};
-                document.getElementById('compiled').innerHTML = template(context);
+                weatherRender(json);
             });
         } else {
             console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
-            var source   = document.getElementById("errorTemplate").innerHTML;
-            var template = Handlebars.compile(source);
             if(response.status === 404){
-                var context = {errorText: "City Not Found"};
+                errorRender("City Not Found");
             }else{
-                var context = {errorText: "Error occurs"};
+                errorRender("Error Occurs");
             }
-            document.getElementById('compiled').innerHTML = template(context);
+
         }
     });
-    return false;
 }
-// stop propagation
+// onclick swap on dom
+// button on submit type
+// function refactoring
