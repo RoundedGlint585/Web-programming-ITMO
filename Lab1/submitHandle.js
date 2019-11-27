c829e8c5ec30027930169d706f4b02266fae365c
roundedglint585 <danonofficial@gmail.com>
2019-11-27 09:29:35 +0300

    Added tests

--- Lab1/submitHandle.js
+++ Lab1/submitHandle.js
@@ -1,6 +1,5 @@
-const form = document.getElementById('inputForm');
-form.onsubmit = function(){
-    event.preventDefault();
-    cityName = document.getElementById('cityName').value;
+const form = document.getElementById('inputForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    const cityName = e.target[0].value;
     weatherFetch(cityName, weatherRender, errorRender);
-};
+});
